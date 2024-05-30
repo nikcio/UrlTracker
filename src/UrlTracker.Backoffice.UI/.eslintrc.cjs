9bf240d95f2a56372145bd6e92a8b4d82eb8a18f
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2024-05-30 22:04:33 +0200

    feat: Add a11y rules

--- src/UrlTracker.Backoffice.UI/.eslintrc.cjs
+++ src/UrlTracker.Backoffice.UI/.eslintrc.cjs
@@ -1,9 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:lit-a11y/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'lit-a11y'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
